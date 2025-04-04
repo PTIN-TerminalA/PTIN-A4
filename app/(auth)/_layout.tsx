5d2288c9a5c70c70a284ac6a3542d4b3424f0fdd
Bruna <bruna.colomer@estudiantat.upc.edu>
2025-04-04 19:46:16 +0200

    Font types updated

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -3,8 +3,14 @@ import { Stack } from "expo-router";
 export default function AuthLayout() {
   return (
     <Stack>
-      <Stack.Screen name="login" options={{ title: "Inicia sessió" }} />
-      <Stack.Screen name="register" options={{ title: "Registra’t" }} />
+      <Stack.Screen
+        name="login"
+        options={{ title: "Inicia sessió", headerShown: false }}
+      />
+      <Stack.Screen
+        name="register"
+        options={{ title: "Registra’t", headerShown: false }}
+      />
     </Stack>
   );
 }
--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -1,9 +1,24 @@
-import { Text, View } from "react-native";
+import { StyleSheet } from "react-native";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
 
 export default function RegisterScreen() {
   return (
-    <View>
-      <Text>Pantalla de registre</Text>
-    </View>
+    <ThemedView style={styles.container}>
+      <ThemedText type="title">Això és un títol</ThemedText>
+      <ThemedText type="subtitle">Això és un subtítol</ThemedText>
+      <ThemedText type="default">Text per defecte (cos)</ThemedText>
+      <ThemedText type="defaultSemiBold">Text semi-bold</ThemedText>
+      <ThemedText type="link">Text d'enllaç</ThemedText>
+    </ThemedView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    gap: 10,
+    paddingTop: 40,
+  },
+});
